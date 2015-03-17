ce1263f81eda20531e5917664746bbd9e01ffd7d
378632268 <378632268@users.noreply.github.com>
2015-03-18 00:28:18 +0800

    大小写错误，已经是第二次被这条注释坑了。
    大小写错误，已经是第二次被这条注释坑了。

--- wechat.class.php
+++ wechat.class.php
@@ -7,8 +7,8 @@
  *  usage:
  *   $options = array(
  *			'token'=>'tokenaccesskey', //填写你设定的key
- *			'encodingaeskey'=>'encodingaeskey', //填写加密用的EncodingAESKey
- *			'appid'=>'wxdk1234567890', //填写高级调用功能的app id
+ *			'encodingAesKey'=>'encodingAesKey', //填写加密用的encodingAESKey
+ *			'appid'=>'wxdk1234567890', //填写高级调用功能的appid
  *			'appsecret'=>'xxxxxxxxxxxxxxxxxxx' //填写高级调用功能的密钥
  *		);
  *	 $weObj = new Wechat($options);
